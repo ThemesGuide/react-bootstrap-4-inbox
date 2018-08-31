c8b12b732e725762faec5405ea0381b76c278145
ThemesGuide <39340428+ThemesGuide@users.noreply.github.com>
2018-08-31 08:27:06 -0400

    changed header

---
+++ src/components/templates/HeaderHtml.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+export const HeaderHtml = () => {
+  return (
+    <header className="bg-primary">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 mx-auto py-2">
+            <div className="card border-0 bg-transparent">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-auto col-4 mx-auto text-nowrap text-center px-3">
+                    <a href="http://themes.guide" target="_new">
+                      <span className="fab fa-react display-1 text-white d-block mx-auto" />
+                    </a>
+                  </div>
+                  <div className="col-lg text-center text-lg-left">
+                    <h1 className="font-weight-light d-inline h2">
+                      <span className="text-dark">React Inbox App</span>
+                    </h1>
+                    <h6>A working template with Bootstrap 4</h6>
+                    <span className="badge badge-light badge-pill mr-1">
+                      reactjs
+                    </span>
+                    <span className="badge badge-light badge-pill">
+                      bootstrap-4
+                    </span>
+                  </div>
+                  <div className="col-lg-3 text-lg-right text-center d-flex flex-lg-column flex-row justify-content-center">
+                    <a
+                      href="http://themes.guide"
+                      className="mt-auto text-white"
+                      target="_new"
+                    >
+                      by Themes.guide
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderHtml;
--- src/index.js
+++ src/index.js
@@ -2,52 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
 import Inbox from "./components/Inbox";
+import HeaderHtml from "./components/templates/HeaderHtml";
 
 function App() {
   return (
     <div>
-      <header className="bg-primary">
-        <div className="container">
-          <div className="row">
-            <div className="col-12 mx-auto py-2">
-              <div className="card border-0 bg-transparent">
-                <div className="card-body">
-                  <div className="row">
-                    <div className="col-lg-auto col-4 mx-auto text-nowrap text-center px-3">
-                      <a href="http://themes.guide" target="_new">
-                        <span className="fab fa-react display-1 text-white d-block mx-auto" />
-                      </a>
-                    </div>
-                    <div className="col-lg text-center text-lg-left">
-                      <h3 className="font-weight-light d-inline">
-                        <span className="text-dark">
-                          React + Bootstrap 4 Inbox App
-                        </span>
-                      </h3>
-                      <h6>A working example template</h6>
-                      <span className="badge badge-light badge-pill mr-1">
-                        reactjs
-                      </span>
-                      <span className="badge badge-light badge-pill">
-                        bootstrap-4
-                      </span>
-                    </div>
-                    <div className="col-lg-3 text-lg-right text-center d-flex flex-lg-column flex-row justify-content-center">
-                      <a
-                        href="http://themes.guide"
-                        className="mt-auto text-white"
-                        target="_new"
-                      >
-                        by Themes.guide
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
+      <HeaderHtml />
       <div className="container">
         <Inbox />
       </div>
